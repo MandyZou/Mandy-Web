16472527d7095d5191e21a64411903342df1310e
Mandy Zou <mz962@mail.missouri.edu>
2017-06-29 01:09:55 -0400

    Change color, add timeline

--- travelMap/twocolumn.js
+++ travelMap/twocolumn.js
@@ -30,8 +30,12 @@ $(document).ready(function(){
         if (scrollBarPosition >= citiesPosition.top){
             getCities.addClass("afterScroll");
             getCities.addClass('animated fadeIn');
+            $(".Sichuan").removeClass("selected02");
             $(".Jiangxi").addClass("selected02");
             $(".Jiangxi").addClass('animated fadeIn');
+            $("#text::before").css({"height":"20%"});
+            $("#16Jan").text("2016.01");
+            // $("#16Jan").addClass("animated flash");
             // console.log("add!")
         } else {
             $(".Jiangxi").removeClass('animated fadeIn');
@@ -50,11 +54,12 @@ $(document).ready(function(){
         if (scrollBarPosition >= (citiesPosition2.top + 20)){
             getCities2.addClass("afterScroll");
             getCities2.addClass("animated fadeIn");
-            $(".Jiangxi").addClass("selected03");
+            $(".Jiangxi").removeClass("selected02");
+            $(".Jiangxi").addClass("selected02");
             $(".Jiangxi").addClass('animated fadeIn');
-            $(".Hubei").addClass("selected03");
+            $(".Hubei").addClass("selected02");
             $(".Hubei").addClass('animated fadeIn');
-            // console.log("add2!")
+            $("#16Jan").text("2016.03");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -67,13 +72,14 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition3.top + 40)){
+        if (scrollBarPosition >= (citiesPosition3.top + 50)){
             getCities3.addClass("afterScroll");
             getCities3.addClass("animated fadeIn");
-            $(".Jiangxi").removeClass("selected03");
-            $(".Hubei").removeClass("selected03");
-            $(".Beijing").addClass("selected04");
+            $(".Jiangxi").removeClass("selected02");
+            $(".Hubei").removeClass("selected02");
+            $(".Beijing").addClass("selected02");
             $(".Beijing").addClass('animated fadeIn');
+            $("#16Jan").text("2016.05");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -86,12 +92,13 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition4.top + 60)){
+        if (scrollBarPosition >= (citiesPosition4.top + 80)){
             getCities4.addClass("afterScroll");
             getCities4.addClass("animated fadeIn");
-            $(".Beijing").removeClass("selected04");
-            $(".Jiangxi").addClass("selected05");
+            $(".Beijing").removeClass("selected02");
+            $(".Jiangxi").addClass("selected02");
             $(".Jiangxi").addClass('animated fadeIn');
+            $("#16Jan").text("2016.07");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -104,12 +111,13 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition5.top + 80)){
+        if (scrollBarPosition >= (citiesPosition5.top + 100)){
             getCities5.addClass("afterScroll");
             getCities5.addClass("animated fadeIn");
-            $(".Jiangxi").removeClass("selected05");
-            $(".Shanxi").addClass("selected06");
+            $(".Jiangxi").removeClass("selected02");
+            $(".Shanxi").addClass("selected02");
             $(".Shanxi").addClass('animated fadeIn');
+            $("#16Jan").text("2016.08");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -122,11 +130,13 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition6.top + 100)){
+        if (scrollBarPosition >= (citiesPosition6.top + 120)){
             getCities6.addClass("afterScroll");
             getCities6.addClass("animated fadeIn");
-            $(".Jiangxi").addClass("selected07");
+            $(".Shanxi").removeClass("selected02");
+            $(".Jiangxi").addClass("selected02");
             $(".Jiangxi").addClass('animated fadeIn');
+            $("#16Jan").text("2016.10");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -139,16 +149,17 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition7.top + 120)){
+        if (scrollBarPosition >= (citiesPosition7.top + 140)){
             getCities7.addClass("afterScroll");
             getCities7.addClass("animated fadeIn");
-            $(".Jiangxi").removeClass("selected07");
-            $(".Hongkong").addClass("selected08");
+            $(".Jiangxi").removeClass("selected02");
+            $(".Hongkong").addClass("selected02");
             $(".Hongkong").addClass('animated fadeIn');
-            $(".Macao").addClass("selected08");
+            $(".Macao").addClass("selected02");
             $(".Macao").addClass('animated fadeIn');
-            $(".Guangdong").addClass("selected08");
+            $(".Guangdong").addClass("selected02");
             $(".Guangdong").addClass('animated fadeIn');
+            $("#16Jan").text("2017.01");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -161,13 +172,14 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition8.top + 140)){
+        if (scrollBarPosition >= (citiesPosition8.top + 160)){
             getCities8.addClass("afterScroll");
             getCities8.addClass("animated fadeIn");
-            $(".Hongkong").removeClass("selected08");
-            $(".Macao").removeClass("selected08");
-            $(".Guangdong").removeClass("selected08");
+            $(".Hongkong").removeClass("selected02");
+            $(".Macao").removeClass("selected02");
+            $(".Guangdong").removeClass("selected02");
             $(".Liaoning").addClass("selected02");
+            $("#16Jan").text("2017.03");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -180,11 +192,12 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition9.top + 160)){
+        if (scrollBarPosition >= (citiesPosition9.top + 200)){
             getCities9.addClass("afterScroll");
             getCities9.addClass("animated fadeIn");
-            $(".Jiangxi").addClass("selected03");
+            $(".Jiangxi").addClass("selected02");
             $(".Liaoning").removeClass("selected02");
+            $("#16Jan").text("2017.04");
         } else {
             // s.removeClass("afterScroll");
         }
@@ -197,11 +210,12 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scrollBarPosition = $(window).scrollTop();
 
-        if (scrollBarPosition >= (citiesPosition10.top + 180)){
+        if (scrollBarPosition >= (citiesPosition10.top + 330)){
             getCities10.addClass("afterScroll");
             getCities10.addClass("animated fadeIn");
-            $(".Sichuan").addClass("selected04");
-            $(".Jiangxi").removeClass("selected03");
+            $(".Sichuan").addClass("selected02");
+            $(".Jiangxi").removeClass("selected02");
+            $("#16Jan").text("2017.06");
         } else {
             // s.removeClass("afterScroll");
         }
