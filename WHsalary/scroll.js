13df8698eab3f0bb8d71e2eb5d8241808fbf236d
Mandy Zou <mz962@mail.missouri.edu>
2017-07-15 20:44:31 -0400

    Add text

--- WHsalary/scroll.js
+++ WHsalary/scroll.js
@@ -8,12 +8,23 @@ $(document).ready(function(){
         if (scrollBarPosition >= tMapPosition.top + 20){
             return simulation.force("x", ForceXCategory)
                             .force("y", ForceYCategory)
-                            .alphaTarget(0.3)
+                            .alphaTarget(0.18)
                             .restart();
         } else {
             return simulation.force("x", ForceXCombine)
                         .force("y", d3.forceY(300).strength(0.08))
                         .force("colide", d3.forceCollide(8));
+        }
+    });
+
+    $(window).scroll(function(){
+        var scrollBarPosition = $(window).scrollTop();
+        if (scrollBarPosition >= tMapPosition.top + 20){
+            $(".text-all").attr("visibility","hidden");
+            $(".text-category").attr("visibility","visible");
+        } else {
+            $(".text-all").attr("visibility","visible");
+            $(".text-category").attr("visibility","hidden");
         }
     });
 
