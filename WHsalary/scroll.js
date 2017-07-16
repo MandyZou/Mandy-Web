ec8c53c7a0b661447396fa576ed642e88f484674
Mandy Zou <mz962@mail.missouri.edu>
2017-07-16 10:06:48 -0400

    Add chart to scroll

--- WHsalary/scroll.js
+++ WHsalary/scroll.js
@@ -30,4 +30,33 @@ $(document).ready(function(){
         }
     });
 
+    //For chart
+    $(window).scroll(function(){
+        var scrollBarPosition = $(window).scrollTop();
+        if (scrollBarPosition >= tMapPosition.top + 500){
+            return simulation.force("x", ForceXChart)
+                            .force("y", ForceYChart)
+                            .alphaTarget(0.18)
+                            .restart();
+            // console.log("hey!")
+        } else {
+            // return simulation.force("x", ForceXCombine)
+            //                 .force("y", d3.forceY(200).strength(0.08))
+            //                 .force("colide", d3.forceCollide(8))
+            //                 .alphaTarget(0.05)
+            //                 .restart();
+        }
+    });
+
+    // $(window).scroll(function(){
+    //     var scrollBarPosition = $(window).scrollTop();
+    //     if (scrollBarPosition >= tMapPosition.top + 800){
+    //         $(".text-all").attr("visibility","hidden");
+    //         $(".text-category").attr("visibility","visible").addClass('animated flipInX');
+    //     } else {
+    //         $(".text-all").attr("visibility","visible").addClass('animated flipInX');
+    //         $(".text-category").attr("visibility","hidden");
+    //     }
+    // });
+
     });
