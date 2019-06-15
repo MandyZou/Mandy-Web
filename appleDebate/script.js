9a93c6641043b0fc72dbf7decd21001200a699d2
Mandy Zou <mz962@mail.missouri.edu>
2019-06-15 14:35:16 +0800

    upload appleDebate

---
+++ appleDebate/script.js
@@ -0,0 +1,10 @@
+// console.log('hello world')
+
+// $('.beginButton').click(function () {
+//     // Code to do scrolling happens here
+//     console.log('clicked!')
+// });
+
+$('.beginButton').click(function(){
+    console.log('hahahahaha')
+});
